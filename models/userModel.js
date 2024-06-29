092e871c021805f33c105498304acf344ce0b5c2
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2024-06-29 18:36:35 +0530

    chanage in models

--- models/userModel.js
+++ models/userModel.js
@@ -5,12 +5,12 @@ const userSchema = new mongoose.Schema(
     role: {
       type: String,
       required: [true, "role is required"],
-      enum: ["admin", "organisation", "user", "hospital"],
+      enum: ["admin", "organisation", "donar", "hospital"],
     },
     name: {
       type: String,
       required: function () {
-        if (this.role === "user" || this.role === "admin") {
+        if (this.role === "donar" || this.role === "admin") {
           return true;
         }
         return false;
@@ -58,4 +58,4 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-module.exports = mongoose.model("users", userSchema);
+module.exports = mongoose.model("donar", userSchema);
