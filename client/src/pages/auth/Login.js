e8ce0ec4684cc1f32d98cd7d99607e46c66317e0
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2024-07-08 23:52:53 +0530

    Login Form & Input Type

--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
+import InputType from "../../components/shared/Form/InputType";
 
 const Login = () => {
   return (
     <div>
-        <h1>Login Page</h1>
+      <div className="row">
+        <div className="col-md-8 form-banner">
+          <img src="./assets/images/banner1.jpg" alt="login"></img>
+        </div>
+        <div className="col-md-4 form-container">
+          <form>
+            <InputType
+              labelText={"Email"}
+              labelFor={"ForMail"}
+              inputType={"email"}
+              name={"email"}
+            />
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Login;
