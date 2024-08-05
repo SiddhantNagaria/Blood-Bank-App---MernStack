5183c1b984c5411e1d8ec7ed3574e536f50186d4
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2024-08-05 23:18:22 +0530

    layout and logout

--- client/src/redux/features/auth/authSlice.js
+++ client/src/redux/features/auth/authSlice.js
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { userLogin } from "./authActions";
+import { getCurrentUser, userLogin, userRegister } from "./authActions";
 
 const token = localStorage.getItem("token")
   ? localStorage.getItem("token")
@@ -17,6 +17,7 @@ const authSlice = createSlice({
   initialState: initialState,
   reducers: {},
   extraReducers: (builder) => {
+    // login user
     builder.addCase(userLogin.pending, (state) => {
       state.loading = true;
       state.error = null;
@@ -27,6 +28,32 @@ const authSlice = createSlice({
       state.token = payload.token;
     });
     builder.addCase(userLogin.rejected, (state, { payload }) => {
+      state.loading = false;
+      state.error = payload;
+    });
+    // REGISTER user
+    builder.addCase(userRegister.pending, (state) => {
+      state.loading = true;
+      state.error = null;
+    });
+    builder.addCase(userRegister.fulfilled, (state, { payload }) => {
+      state.loading = false;
+      state.user = payload.user;
+    });
+    builder.addCase(userRegister.rejected, (state, { payload }) => {
+      state.loading = false;
+      state.error = payload;
+    });
+    // CURRENT user
+    builder.addCase(getCurrentUser.pending, (state) => {
+      state.loading = true;
+      state.error = null;
+    });
+    builder.addCase(getCurrentUser.fulfilled, (state, { payload }) => {
+      state.loading = false;
+      state.user = payload.user;
+    });
+    builder.addCase(getCurrentUser.rejected, (state, { payload }) => {
       state.loading = false;
       state.error = payload;
     });
